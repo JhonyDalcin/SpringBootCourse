8a4290c21fd5ecfdb55a1401210f25a1c1175126
Jhony Dalcin <jhonydalcin@gmail.com>
2024-11-06 17:22:20 -0300

    Added product get by ID endpoint

--- src/main/java/br/com/digisolutions/productsapi/controller/ProductController.java
+++ src/main/java/br/com/digisolutions/productsapi/controller/ProductController.java
@@ -1,7 +1,10 @@
 package br.com.digisolutions.productsapi.controller;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +32,13 @@ public class ProductController {
         productRepository.save(product);
         System.out.println("Product: " + product);
         return product;
+    }
+
+    @GetMapping("{id}")
+    public Product getProductById(@PathVariable("id") String id) {
+//      Optional<Product> product = productRepository.findById(id);
+//      return product.isPresent() ? product.get() :null;
+        return productRepository.findById(id).orElse(null);
     }
 
 }
