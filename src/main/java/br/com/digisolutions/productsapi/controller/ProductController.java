28cfca88e1e8ed8d1a4e6f4d9a0c2e82903551bc
Jhony Dalcin <jhonydalcin@gmail.com>
2024-11-07 15:58:26 -0300

    Added product update by ID

--- src/main/java/br/com/digisolutions/productsapi/controller/ProductController.java
+++ src/main/java/br/com/digisolutions/productsapi/controller/ProductController.java
@@ -3,9 +3,11 @@ package br.com.digisolutions.productsapi.controller;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +41,20 @@ public class ProductController {
 //      Optional<Product> product = productRepository.findById(id);
 //      return product.isPresent() ? product.get() :null;
         return productRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable("id") String id){
+        productRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void update(@PathVariable("id") String id, 
+                        @RequestBody Product product){
+        
+        product.setId(id);
+        productRepository.save(product);
+
     }
 
 }
